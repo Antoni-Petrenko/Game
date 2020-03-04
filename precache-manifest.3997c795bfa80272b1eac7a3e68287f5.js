837ea11d0fecbed97105c1d23c27ff2a2611a93e
Antoni Petrenko <flay9123@gmail.com>
2020-03-04 15:49:33 +0200

    Updates

---
+++ precache-manifest.3997c795bfa80272b1eac7a3e68287f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3eaa4fb0590dff33e124b60c5de5132",
+    "url": "/Game/index.html"
+  },
+  {
+    "revision": "522420023d077cbbfa9a",
+    "url": "/Game/static/css/main.65743f9d.chunk.css"
+  },
+  {
+    "revision": "2f0ac4a37bc17fa35960",
+    "url": "/Game/static/js/2.24bd5f0e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Game/static/js/2.24bd5f0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "522420023d077cbbfa9a",
+    "url": "/Game/static/js/main.20ac5071.chunk.js"
+  },
+  {
+    "revision": "5c0ff4cde1a6e008608a",
+    "url": "/Game/static/js/runtime-main.08e56f8c.js"
+  }
+]);
